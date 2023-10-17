06b149d2044ead4dcfb7dc088ece7db71fce604b
Marco Vavassori <mv@marcovavassori.com>
2023-10-17 11:34:44 +0200

    robots and sitemap env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "http://localhost:3000", //change to https://your-domain.com
+  siteUrl: process.env.SITE_URL || "http://localhost:3000",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
